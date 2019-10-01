f1886717fcfb8c7f7510725be41a74614f5f58fb
Abhishek Samariya <asamariya@gmail.com>
2019-10-01 19:41:42 +1000

    Fetch from Giphy API
    Able to get response from the Giphy api and convert to json

--- src/App.js
+++ src/App.js
@@ -20,12 +20,32 @@ class App extends Component {
 			hintText: ''
 		};
 	}
+
+	// we want a function that searches the giphy api using fetch
+	// and puts the search term into the query url and
+	// then we can do something with the results
+
+	// we can also write async methods into our components
+	// that let us use the async/await style of functions
+	searchGiphy = async searchTerm => {
+		// first we try our fetch
+		try {
+			// here we use the await keyword to wait for our response to come back
+			const response = await fetch(
+				`https://api.giphy.com/v1/gifs/search?api_key=frzJZgXDzDP5ezre7rfuRIhWvfovmaUY&q=${searchTerm}&limit=25&offset=0&rating=G&lang=en`
+			);
+			// here we convert our raw resonse into json data
+			const data = await response.json();
+			console.log(data);
+			// if our fetch fails, we catch it down here
+		} catch (error) {}
+	};
 	handleKeyPress = event => {
 		const {value} = event.target;
 		// When we have 2 or more characters in our search box
 		// and we have also pressed enter, we then want to run a search
 		if (value.length > 2 && event.key === 'Enter') {
-			alert(`search for ${value}`);
+			this.searchGiphy(value);
 		}
 	};
 
